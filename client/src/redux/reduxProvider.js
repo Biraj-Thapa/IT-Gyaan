bf2c9a7b95c927058b1226a2ded2eb4cfebe595e
Biraj Thapa <birajthapa983@gmail.com>
2024-02-19 15:21:42 +0545

    added reduxpersist

--- client/src/redux/reduxProvider.js
+++ client/src/redux/reduxProvider.js
@@ -1,12 +1,15 @@
-"use client"
+'use client'
 import { Provider } from 'react-redux';
-import store from './store';
-
-const ReduxProvider =({children})=>{
-    return(
+import { persistor, store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+const ReduxProvider = ({children})=>{
+    return (
         <Provider store={store}>
-            {children}
-        </Provider>
+              <PersistGate loading={null} persistor={persistor}>
+              {children}
+              </PersistGate>
+            </Provider>
     )
 }
-export default ReduxProvider;
+
+export default ReduxProvider
